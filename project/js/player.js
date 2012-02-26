3cadce39f67040dddbdc650fb97f5e03b8800cc8
Florent Lavy <flo.lavy@gmail.com>
2012-02-26 19:24:03 +0100

    Started working on player component

---
+++ project/js/player.js
@@ -0,0 +1,31 @@
+Crafty.c('Player', {
+    color: 'black',
+    playerNumber: 0,
+    spawnLines: [],
+    units: [],
+
+    player: function(playerNumber) {
+        this.playerNumber = playerNumber;
+        if(playerNumber === 1) {
+            this.color = 'blue';
+        }
+        else if (playerNumber === 2) {
+            this.color = 'orange';
+        }
+        else {
+            throw 'Player number out of range !';
+        }
+        this._setSpawnLines();
+        return this;
+    },
+
+    _setSpawnLines: function() {
+        if(this.playerNumber === 1) {
+            this.spawnLines[] = 9;
+        }
+        else
+        {
+            this.spawnLines[] = 0;
+        }
+    }
+});
