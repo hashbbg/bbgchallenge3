d801adc8eefc0e919d3801eee241adfcc00eb3d1
Adrian Gaudebert <adrian@gaudebert.fr>
2012-02-26 18:15:10 +0100

    Changed cell dimensions.

--- project/js/config.js
+++ project/js/config.js
@@ -5,7 +5,7 @@ REACH.config = {
     width: 10,  // number of cells
     height: 10,  // number of cells
     cell: {
-        width: 64,  // in pixels
-        height: 64  // in pixels
+        width: 101,  // in pixels
+        height: 171  // in pixels
     }
 };
