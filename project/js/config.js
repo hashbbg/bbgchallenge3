4ce77314dee4a710c9820c5370d9050faf72ee63
Adrian Gaudebert <adrian@gaudebert.fr>
2012-02-26 16:31:56 +0100

    Finished creating template.

---
+++ project/js/config.js
@@ -0,0 +1,11 @@
+// Namespace
+REACH = window.REACH || {};
+
+REACH.config = {
+    width: 10,  // number of cells
+    height: 10,  // number of cells
+    cell: {
+        width: 64,  // in pixels
+        height: 64  // in pixels
+    }
+};
---
+++ project/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    var stageHeight = REACH.config.height * REACH.config.cell.height,
+        stageWidth = REACH.config.width * REACH.config.cell.width;
+
+    Crafty.init(stageWidth, stageHeight);
+
+    Crafty.scene('loading', function() {
+        Crafty.background('#fff');
+        Crafty.e('2D, DOM, Text').attr({ w: 100, h: 20, x: 150, y: 120 })
+                .text('Loading')
+                .css({ 'text-align': 'center' });
+    });
+
+    Crafty.scene('loading');
+});
