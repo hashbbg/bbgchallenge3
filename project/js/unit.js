0bb072cc531d9e53cb25a7b509c2216ce5efd89a
Florent Lavy <flo.lavy@gmail.com>
2012-02-26 19:26:09 +0100

    Started working on unit component

---
+++ project/js/unit.js
@@ -0,0 +1,26 @@
+Crafty.c('Unit', {
+    gridX: 0,
+    gridY: 0,
+    healthPoint: 1,
+    attackValue: 1,
+
+    init: function() {
+        this.requires('2D, DOM');
+    },
+
+    unit: function() {
+        return this;
+    },
+
+    move: function() {
+
+    },
+
+    attack: function() {
+
+    },
+
+    die: function() {
+
+    }
+});
