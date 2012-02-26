e740d6f74c876e5baba32a1e6edc1785ad76e42c
Florent Lavy <flo.lavy@gmail.com>
2012-02-26 20:25:41 +0100

    Added getter of list of accessible cells in unit component

--- project/js/unit.js
+++ project/js/unit.js
@@ -3,12 +3,14 @@ Crafty.c('Unit', {
     gridY: 0,
     healthPoint: 1,
     attackValue: 1,
+    player: null,
 
     init: function() {
         this.requires('2D, DOM');
     },
 
-    unit: function() {
+    unit: function(player) {
+        this.player = player;
         return this;
     },
 
@@ -22,5 +24,24 @@ Crafty.c('Unit', {
 
     die: function() {
 
+    },
+
+    getListOfAccessibleCells: function() {
+        var yDelta = -1;
+        var accessibleCells = [];
+        if (this.player.playerNumber === 1) {
+            yDelta = 1;
+        }
+
+        if (this.gridY + yDelta >= 0 && this.gridY + yDelta < REACH.config.height) {
+            accessibleCells.push([this.gridX, this.gridY + yDelta]);
+        }
+
+        if (this.gridX > 0) {
+            accessibleCells.push([this.gridX - 1, this.gridY]);
+        }
+        if (this.gridX < REACH.config.width - 1) {
+            accessibleCells.push([this.gridX + 1, this.gridY]);
+        }
     }
 });
