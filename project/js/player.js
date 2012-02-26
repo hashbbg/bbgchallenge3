8a2506ef1fb258859f636202aaab0072ec2e3dc1
Adrian Gaudebert <adrian@gaudebert.fr>
2012-02-26 20:47:35 +0100

    Fixed bug with spawnList generation.

--- project/js/player.js
+++ project/js/player.js
@@ -1,36 +1,46 @@
-Crafty.c('Player', {
-    color: 'black',
-    playerNumber: 0,
-    spawnCells: [],
-    units: [],
+(function(Crafty, REACH) {
 
-    player: function(playerNumber) {
-        this.playerNumber = playerNumber;
+    var generateSpawnCells = function(playerNumber) {
+        var indexY, indexX,
+            spawnCells = [];
         if (playerNumber === 1) {
-            this.color = 'blue';
-        }
-        else if (playerNumber === 2) {
-            this.color = 'orange';
-        }
-        else {
-            throw 'Player number out of range !';
-        }
-        this._initSpawnCells();
-        return this;
-    },
-
-    _initSpawnCells: function() {
-        var indexY;
-        if (this.playerNumber === 1) {
             indexY = REACH.config.height - 1;
         }
         else {
             indexY = 0;
         }
 
-        for (indexX = 0; indexX < REACH.config.width - 1; indexX++)
-        {
-            this.spawnCells.push([indexX, indexY]);
+        for (indexX = 0; indexX < REACH.config.width; indexX++) {
+            spawnCells.push([indexX, indexY]);
         }
+
+        return spawnCells;
     }
-});
+
+    Crafty.c('Player', {
+        color: 'black',
+        playerNumber: 0,
+        spawnCells: [],
+        units: [],
+
+        player: function(playerNumber) {
+            this.playerNumber = playerNumber;
+            if (playerNumber === 1) {
+                this.color = 'blue';
+            }
+            else if (playerNumber === 2) {
+                this.color = 'orange';
+            }
+            else {
+                throw 'Player number out of range!';
+            }
+            this.spawnCells = generateSpawnCells(this.playerNumber);
+            return this;
+        },
+
+        getListOfAccessibleCells: function() {
+            return this.spawnCells;
+        },
+    });
+
+})(window.Crafty, window.REACH);
